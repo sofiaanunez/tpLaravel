0a9f661dcc8211436c1f9b16ab25d511d306eeab
Digital House <maxicopp@gmail.com>
2018-07-18 16:17:39 -0300

    16-17

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('plantilla-final.plantillaGeneral')
 
 @section('contenido')
+  <br>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -66,4 +67,5 @@
         </div>
     </div>
 </div>
+<br>
 @endsection
--- resources/views/plantillaIndex/headerIndex.blade.php
+++ resources/views/plantillaIndex/headerIndex.blade.php
@@ -21,23 +21,48 @@
                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
              </li>
          @else
-             <li class="nav-item dropdown">
-                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                     {{ Auth::user()->name }} <span class="caret"></span>
-                 </a>5t
-
-                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                     <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                      document.getElementById('logout-form').submit();">
-                         {{ __('Logout') }}
-                     </a>
-
+               <li>
+                 <a class="dropdown-item" href="{{ route('logout') }}">{{ __('Cerrar sesión') }} </a>
+               </li>
                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                          @csrf
                      </form>
-                 </div>
-             </li>
+
+                     {{-- <li class="nav-item">
+                         <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                       </li>
+
+
+                       <ul class="nav nav-pills">
+                         <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                           <div class="dropdown-menu">
+                             <a class="dropdown-item" href="#">Mi perfil</a>
+                             <div class="dropdown-divider"></div>
+                             <a class="dropdown-item" href="#">Cerrar sesión</a>
+                           </div>
+                         </li>
+
+                       </ul> --}}
+
+                       <li class="nav-item dropdown">
+                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              {{ Auth::user()->name }} <span class="caret"></span>
+
+                          </a>
+
+                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="{{ route('logout') }}"
+                                 onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                  {{ __('Logout') }}
+                              </a>
+
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                  @csrf
+                              </form>
+                          </div>
+                      </li>
          @endguest
      </ul>
    </nav>
