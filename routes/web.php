b51a80dcb9935161297fb979abec7086dfc541d7
FUSER93 <37194695+FUSER93@users.noreply.github.com>
2018-07-22 20:58:54 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,20 @@ Route::get('/login', 'controladorGeneral@login');
 
 Route::get('/faq', 'controladorGeneral@faq');
 
+Route::get('/register', 'storageController@index');
+Route::post('/storage/create', 'storageController@save');
+
+Route::get('storage/{archivo}', function ($archivo) {
+     $public_path = public_path();
+     $url = $public_path.'/storage/'.$archivo;
+     //verificamos si el archivo existe y lo retornamos
+     if (Storage::exists($archivo))
+     {
+       return response()->download($url);
+     }
+     //si no se encuentra lanzamos un error 404.
+     abort(404);
+
+});
+
 Auth::routes();
